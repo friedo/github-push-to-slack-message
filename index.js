d033d25226a8107f200bf457a20925c6ed8548c5
Mike Friedman <friedo@friedo.com>
2016-02-29 14:28:18 -0500

    adding attachments

--- index.js
+++ index.js
@@ -9,12 +9,23 @@ module.exports = {
         var before    = step.input( 'before' ).first();
         var after     = step.input( 'after' ).first();
         var compare   = step.input( 'compare' ).first();
-        var commits   = step.input( 'commits' ).toArray();
+        var commits   = step.input( 'commits' ).first().toArray();
         var repo      = step.input( 'repository' ).first();
         var sender    = step.input( 'sender' ).first();
 
         var message = {
             "text": '<' + sender.html_url + '|@' + sender.login + '> pushed ' + commits.length + ' commits to ' + '<' + repo.html_url + '|' + repo.full_name + '>'
+            "attachments": [ {
+                'fallback': 'fallback text',
+                'color': '#00ffff',
+                'author_name': '@' + sender.login,
+                'author_link': sender.html_url,
+                'author_icon': sender.avatar_url,
+                'title': 'View changes',
+                'title_link': compare,
+                'text': "```\nthis should be formatted\n```",
+                "mrkdwn_in": [ 'text' ]
+            } ]
         };
 
         this.complete( message );
